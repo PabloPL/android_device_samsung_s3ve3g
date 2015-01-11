b885703b7b01f878c6165b3ab1fa2d0b07807da6
Paweł Chmiel <pawel.mikolaj.chmiel@gmail.com>
2015-01-11 23:32:25 +0100

    Don't copy twrp.fstab to etc - it don't work with cm sources. Use TARGET_RECOVERY_FSTAB :/.

--- device.mk
+++ device.mk
@@ -39,10 +39,6 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     ueventd.qcom.rc
 
-# TWRP
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/twrp.fstab:recovery/root/etc/twrp.fstab
-
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8226.conf:system/etc/thermal-engine-8226.conf
